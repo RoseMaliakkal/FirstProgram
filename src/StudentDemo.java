b8198fffbe5c66b05aadf0c1a3f36580b2293930
Aleena <rosemalikkal1999@gmail.com>
2023-03-15 01:01:49 +0530

    this keyword explained

---
+++ src/StudentDemo.java
@@ -0,0 +1,17 @@
+public class StudentDemo {
+    int rollNumber;
+    String name;
+    String Standard;
+    public StudentDemo(int rollNumber,String name, String Standard){
+        this.rollNumber=rollNumber;
+        this.name=name;
+        this.Standard=Standard;
+    }
+    public void display(){
+        System.out.println("My name is "+name+"\nMy roll number is "+rollNumber+"\nMy standard is "+Standard);
+    }
+    public static void main(String[] args){
+        StudentDemo obj=new StudentDemo(123,"Aleena","5th");
+        obj.display();
+    }
+}
